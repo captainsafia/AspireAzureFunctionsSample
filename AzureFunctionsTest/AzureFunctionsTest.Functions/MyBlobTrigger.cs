3c4e62a752beba1c0e669a346bdc6dbb9e3aef79
Safia Abdalla <safia@safia.rocks>
2024-09-26 22:04:45 -0700

    Update packages for deployment to ACA

--- AzureFunctionsTest/AzureFunctionsTest.Functions/MyBlobTrigger.cs
+++ AzureFunctionsTest/AzureFunctionsTest.Functions/MyBlobTrigger.cs
@@ -1,15 +1,15 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
+// using Microsoft.Azure.Functions.Worker;
+// using Microsoft.Extensions.Logging;
 
-namespace AzureFunctionsTest.Functions;
+// namespace AzureFunctionsTest.Functions;
 
-public class MyBlobTrigger(ILogger<MyBlobTrigger> logger)
-{
-    [Function(nameof(MyBlobTrigger))]
-    [BlobOutput("test-files/{name}.txt", Connection = "blob")]
-    public string Run([BlobTrigger("blobs/{name}", Connection = "blob")] string triggerString)
-    {
-        logger.LogInformation("C# blob trigger function invoked with {message}...", triggerString);
-        return triggerString.ToUpper();
-    }
-}
+// public class MyBlobTrigger(ILogger<MyBlobTrigger> logger)
+// {
+//     [Function(nameof(MyBlobTrigger))]
+//     [BlobOutput("test-files/{name}.txt", Connection = "blob")]
+//     public string Run([BlobTrigger("blobs/{name}", Connection = "blob")] string triggerString)
+//     {
+//         logger.LogInformation("C# blob trigger function invoked with {message}...", triggerString);
+//         return triggerString.ToUpper();
+//     }
+// }
