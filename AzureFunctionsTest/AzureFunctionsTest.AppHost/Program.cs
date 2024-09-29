5921002f3f00b4c18abab434d9edb2925863d980
Safia Abdalla <safia@safia.rocks>
2024-09-29 15:56:35 -0700

    Update with working deployment

--- AzureFunctionsTest/AzureFunctionsTest.AppHost/Program.cs
+++ AzureFunctionsTest/AzureFunctionsTest.AppHost/Program.cs
@@ -3,12 +3,7 @@ using Azure.Provisioning.Storage;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-#pragma warning disable AZPROVISION001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-var storage = builder.AddAzureStorage("storage", (builder, construct, storageAccount) =>
-{
-    construct.Add(storageAccount.AssignRole(StorageBuiltInRole.StorageAccountContributor, construct.PrincipalTypeParameter, construct.PrincipalIdParameter));
-}).RunAsEmulator();
-#pragma warning restore AZPROVISION001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var storage = builder.AddAzureStorage("storage").RunAsEmulator();
 var queue = storage.AddQueues("queue");
 var blob = storage.AddBlobs("blob");
 
@@ -20,15 +15,7 @@ var funcApp = builder.AddAzureFunctionsProject<Projects.AzureFunctionsTest_Funct
     .WithReference(eventHubs)
     .WithReference(serviceBus)
     .WithReference(blob)
-    .WithReference(queue)
-    .WithHostStorage(storage)
-    .WithEnvironment(context =>
-    {
-        // Function's Docker images are configured to listen on privileged port 
-        // :80 by default. We override that here using the environment variable
-        // that is respected by the classic WebHostBuilder.
-        context.EnvironmentVariables["ASPNETCORE_URLS"] = "http://+:8080";
-    });
+    .WithReference(queue);
 
 var apiService = builder.AddProject<Projects.AzureFunctionsTest_ApiService>("apiservice")
     .WithExternalHttpEndpoints()
